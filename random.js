17df9c80c5029f7ffb0456167e62995da94c4dd5
SUMAYA2 <sumayaislamsumi2020@gmail.com>
2020-10-12 22:48:50 +0600

    2nd commit

---
+++ random.js
@@ -0,0 +1,15 @@
+var num=2.4902458;
+var result=Math.floor(num);
+
+var result2=Math.ceil(num);
+var result3=Math.round(num);
+var randomnumber =Math.random()*10;
+var output=Math.round(randomnumber);
+console.log(output);
+
+//using for loop
+for(i=0;i<=10;i++){
+    var randomnumber =Math.random()*10;
+var output=Math.round(randomnumber);
+console.log(output);
+}
